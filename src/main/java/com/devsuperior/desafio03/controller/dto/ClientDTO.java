100eaca15ceaf6502e767618f39bf7f94b826422
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-25 17:07:40 -0300

    pacote dto

---
+++ src/main/java/com/devsuperior/desafio03/controller/dto/ClientDTO.java
@@ -0,0 +1,60 @@
+package com.devsuperior.desafio03.controller.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.desafio03.entities.Client;
+
+public class ClientDTO {
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private LocalDate birthDate;
+	private Integer children;
+	
+	public ClientDTO() {
+	}
+	
+	public ClientDTO(Client entity) {
+		id = entity.getId();
+		name = entity.getName();
+		cpf = entity.getCpf();
+		income = entity.getIncome();
+		birthDate = entity.getBirthDate();
+		children = entity.getChildren();
+	}
+
+	public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+}
